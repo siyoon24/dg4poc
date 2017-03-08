5455b771498b1d81efc26dbdccb6c75f22f5a815
siyoon24 <dg-user@192.168.47.128>
2017-03-08 23:14:25 +0900

    2017.03.08

---
+++ DG_2017/src/dg_src/Snippet.java
@@ -0,0 +1,8 @@
+package dg_src;
+
+public class Snippet {
+	public static void main(String[] args) {
+		/Library/Java/JavaVirtualMachines/jdk1.8.0_121.jdk/Contents/Home/jre/lib/resources.jar
+	}
+}
+
